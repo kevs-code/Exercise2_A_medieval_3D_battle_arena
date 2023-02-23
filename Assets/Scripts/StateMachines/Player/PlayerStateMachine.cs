3e96d748576cb43bedcf66f371105333f3825d22
Kevin Smith <kevscode@hotmail.com>
2023-02-23 13:44:16 +0000

    Added Attacking data

--- Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -14,6 +14,8 @@ public class PlayerStateMachine : StateMachine
     [field: SerializeField] public float RotationDamping { get; private set; }
     [field: SerializeField] public ForceReceiver ForceReceiver { get; private set; }
 
+    [field: SerializeField] public Attack[] Attacks { get; private set; }
+
     public Transform MainCameraTransform { get; private set; }
     private void Start()
     {
